2c62aa8a461c6a318005d8780cfa18ee19423f67
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-10-18 17:18:22 -0400

    hw

---
+++ app.js
@@ -0,0 +1,37 @@
+var arr = [["Cats", 2], ["Dogs", 4]];
+
+
+
+function multiplyAll(arr) {
+  var product = 1;
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+   return product;
+    }
+  }
+
+
+   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+   console.log(arr(1,2));
+   console.log(arr(3,4));
+   console.log(arr(5,6,7));
+}
+
+
+function testGreaterThan(val) {
+  if (val) { 
+    return "Over 10";
+  }
+
+  if (val) { 
+    return "Over 50";
+  }
+
+  return "5 or Under";
+}
+
+testGreaterThan(3,5);
+console.log(3,5);
+
+
